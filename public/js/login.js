03832443fe758ee6cbf7268e54fb7bf3437d1930
Enrique Collazo <ecollazo1991@gmail.com>
2021-03-27 11:35:44 -0400

    updates links

--- public/js/login.js
+++ public/js/login.js
@@ -15,12 +15,13 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/dashboard');
+      document.location.replace('/dashboard'); 
     } else {
       alert(response.statusText);
     }
   }
 };
+
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
