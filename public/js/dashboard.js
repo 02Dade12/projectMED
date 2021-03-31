b9bf8428f553a37accf93529ca039d709e94b64b
Darius Hay <dariushay@gmail.com>
2021-03-30 20:10:15 -0400

    changes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,16 @@
-var text = document.querySelector('#search').value.trim();
-const newSearchHandler = async (event) => {
+const axios = require("axios");
+var text = document.querySelector('form');
+
+const newSearchHandler = async () => {
   // event.preventDefault();
+var searchText = document.querySelector('#search').value.trim();
 
   // const name = document.querySelector('#project-name').value.trim();
   // const needed_funding = document.querySelector('#project-funding').value.trim();
-  // const description = document.querySelector('#project-desc').value.trim();
-  console.log(text);
 
+
+await stockOverview(searchText);
+await dailySeries(searchText);
 
 
 
@@ -26,6 +30,61 @@ const newSearchHandler = async (event) => {
   //   }
   // }
 };
+function stockOverview(stock) {
+  axios
+    .get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + stock + "&apikey=" + process.env.API_KEY)
+
+    .then(
+      function (response) {
+      
+        console.log("-----------------------")
+        console.log(response.data);
+        console.log("-----------------------")
+        console.log(response.data.Name);
+        console.log(response.data.Symbol);
+        console.log(response.data.Country);
+        console.log(response.data.Exchange);
+        console.log(response.data.Sector);
+        console.log(response.data.Description);
+        console.log("-----------------------")
+
+        let stockName = response.data.Name;
+        let stockSymbol = response.data.Symbol;
+        let stockCountry = response.data.Country;
+        let stockSector = response.data.Exchange;
+        let stockExchange = response.data.Sector;
+        let stockDescription = response.data.Description;
+
+      }
+    )
+};
+
+stockOverview("now");
+
+function dailySeries(stock) {
+  axios
+  .get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + stock + "&apikey=" + process.env.API_KEY)
+
+  .then(
+    function (response) {
+
+      console.log("-----------------------")
+      console.log(response.data);
+      console.log("-----------------------")
+      console.log(response.data["Global Quote"]["02. open"]);
+      console.log(response.data["Global Quote"]["03. high"]);
+      console.log(response.data["Global Quote"]["04. low"]);
+      console.log(response.data["Global Quote"]["05. price"]);
+      console.log("-----------------------")
+
+      let stockOpen = response.data["Global Quote"]["02. open"];
+      let stockHigh = response.data["Global Quote"]["03. high"];
+      let stockLow = response.data["Global Quote"]["04. low"];
+      let stockPrice = response.data["Global Quote"]["05. price"];
+
+    }
+  ); 
+};
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
@@ -43,9 +102,7 @@ const delButtonHandler = async (event) => {
   }
 };
 
-// document
-//   .querySelector('#searchBtn')
-//   .addEventListener('click', newSearchHandler);
+text.addEventListener('submit', newSearchHandler);
 
 // document
 //   .querySelector('.project-list')
